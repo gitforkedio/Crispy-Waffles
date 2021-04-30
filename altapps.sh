eedd067649919a9c81c8bebe2be57c5ca34e7d4d
clickonrefresh <72121107+clickonrefresh@users.noreply.github.com>
2021-04-30 08:24:15 +0200

    Create altapps.sh

---
+++ altapps.sh
@@ -0,0 +1,29 @@
+# Alt Apps
+
+# Odoo
+
+```
+cd clickonbonsai \
+  && docker-compose up -d
+```
+```
+cd clickonodoo \
+  && docker-compose up -d
+```
+
+# Hassio
+
+```
+cd clickonhassio \
+  && docker-compose up -d
+```
+
+------------------------------------------------
+# Global
+
+# Noip Dynamic Update client
+
+```
+chmod +x clickonnoip/noip.sh \
+  && ./clickonnoip/noip.sh
+```
