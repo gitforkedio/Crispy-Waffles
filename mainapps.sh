194dbfd67bcfceccf61248cbe3cc5a0905655179
clickonrefresh <72121107+clickonrefresh@users.noreply.github.com>
2021-04-30 08:24:45 +0200

    Create mainapps.sh

---
+++ mainapps.sh
@@ -0,0 +1,34 @@
+# Main Apps
+---
+
+# Odoo
+
+```
+cd clickonrefresh \
+  && docker-compose up -d
+```
+
+# Bookstack
+
+```
+cd clickondocs \
+  && docker-compose up -d
+```
+
+# Organizr
+
+```
+cd clickondashboard \
+  && docker-compose up -d
+```
+
+------------------------------------------------
+
+# Global
+
+# Noip Dynamic Update client
+
+```
+chmod +x clickonnoip/noip.sh \
+  && ./clickonnoip/noip.sh
+```
