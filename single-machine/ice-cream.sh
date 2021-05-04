029cae1811a63310abf25070fbbb5041d3a04f10
clickonrefresh <72121107+clickonrefresh@users.noreply.github.com>
2021-05-04 23:57:42 +0200

    Update ice-cream.sh

--- single-machine/ice-cream.sh
+++ single-machine/ice-cream.sh
@@ -8,82 +8,52 @@ git clone -b waffle-stack https://github.com/Crispy-Waffles/Waffle-Stack.git \
 
 # Admin Apps
 # Nginx Proxy Manager
- 
-```
- cd nginxproxymanager \
+cd nginxproxymanager \
    && docker-compose up -d \
    && cd ..
-```
 
 # Portainer
-
-```
 chmod +x /portainer/portainer.sh \
-  && sudo bash ./portainer/portainer.sh
-```
+  && sudo bash ./portainer/portainer.sh \
+  && cd ..
 
 # Cockpit
-
-```
 chmod +x cockpit/cockpit.sh \
-  && sudo bash ./cockpit/cockpit.sh
-```
+  && sudo bash ./cockpit/cockpit.sh \
+  && cd ..
 
 # Main Apps
 # Odoo
-
-```
 cd odoo1 \
   && docker-compose up -d \
   && cd ..
-```
 
 # Bookstack
-
-```
 cd bookstack \
   && docker-compose up -d \
   && cd ..
-```
 
 # Organizr
-
-```
 cd organizr \
   && docker-compose up -d \
   cd ..
-```
-
 
 # Alt Apps
 # Odoo
-
-```
 cd odoo2 \
   && docker-compose up -d \
   && cd ..
-```
-
-```
+  
 cd odoo3 \
   && docker-compose up -d \
   && cd ..
-```
 
 # Hassio
-
-```
 cd hassio \
   && docker-compose up -d \
   && cd ..
-```
-
 
 # Global
-
 # Noip Dynamic Update client
-
-```
 chmod +x noip/noip.sh \
   && sudo bash ./noip/noip.sh
-```
