dbd4aa5281af17512685dbc69ac821c06d3215ad
clickonrefresh <72121107+clickonrefresh@users.noreply.github.com>
2021-05-05 01:08:59 +0200

    Update mainapps.sh

--- mainapps/mainapps.sh
+++ mainapps/mainapps.sh
@@ -1,34 +1,26 @@
+!#bin/bash
+
+# Clone the main-apps branch into the VM
+git clone -b main-apps https://github.com/Crispy-Waffles/Waffle-Stack.git \
+  && cd Waffle-Stack
+
 # Main Apps
----
-
 # Odoo
-
-```
 cd odoo1 \
-  && docker-compose up -d
-```
+  && docker-compose up -d \
+  && cd ..
 
 # Bookstack
-
-```
 cd bookstack \
-  && docker-compose up -d
-```
+  && docker-compose up -d \
+  && cd ..
 
 # Organizr
-
-```
 cd organizr \
-  && docker-compose up -d
-```
-
-------------------------------------------------
+  && docker-compose up -d \
+  && cd ..
 
 # Global
-
 # Noip Dynamic Update client
-
-```
 chmod +x noip/noip.sh \
   && ./noip/noip.sh
-```
