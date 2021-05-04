95366e6915d6f7ea9f402da0242b9583726b4b41
clickonrefresh <72121107+clickonrefresh@users.noreply.github.com>
2021-05-05 01:08:15 +0200

    Update altapps.sh

--- altapps/altapps.sh
+++ altapps/altapps.sh
@@ -1,29 +1,25 @@
+!#bin/bash
+
+# Clone the alt-apps branch into the VM
+git clone -b alt-apps https://github.com/Crispy-Waffles/Waffle-Stack.git \
+  && cd Waffle-Stack
+  
 # Alt Apps
-
 # Odoo
-
-```
 cd odoo2 \
-  && docker-compose up -d
-```
-```
+  && docker-compose up -d \
+  && cd ..
+
 cd odoo3 \
-  && docker-compose up -d
-```
+  && docker-compose up -d \
+  && cd ..
 
 # Hassio
-
-```
 cd hassio \
-  && docker-compose up -d
-```
+  && docker-compose up -d \
+  && cd ..
 
-------------------------------------------------
 # Global
-
 # Noip Dynamic Update client
-
-```
 chmod +x noip/noip.sh \
   && ./noip/noip.sh
-```
