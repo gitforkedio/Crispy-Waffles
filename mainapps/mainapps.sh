dd4b97bf05f315acd8de419e3c8b54b8a4394aac
clickonrefresh <72121107+clickonrefresh@users.noreply.github.com>
2021-05-11 04:32:03 +0200

    Update mainapps.sh

--- mainapps/mainapps.sh
+++ mainapps/mainapps.sh
@@ -1,8 +1,8 @@
-!#bin/bash
+#!bin/bash
 
 # Clone the main-apps branch into the VM
-git clone -b main-apps https://github.com/Crispy-Waffles/Waffle-Stack.git \
-  && cd Waffle-Stack
+git clone -b main-apps https://github.com/Crispy-Waffles/Crispy-Waffles.git \
+  && cd Crispy-Waffles
 
 # Main Apps
 # Odoo
@@ -22,5 +22,6 @@ cd organizr \
 
 # Global
 # Noip Dynamic Update client
-chmod +x noip/noip.sh \
-  && ./noip/noip.sh
+# cd noip \
+#   && chmod +x noip.sh \
+#   && bash noip.sh
